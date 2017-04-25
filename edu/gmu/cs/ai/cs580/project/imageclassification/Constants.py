12ca0fd41a9c088ba7b34714165dcb598b876817
William Austin <william.r.austin@gmail.com>
2017-04-25 12:20:02 -0400

    Updates for model generation.

--- edu/gmu/cs/ai/cs580/project/imageclassification/Constants.py
+++ edu/gmu/cs/ai/cs580/project/imageclassification/Constants.py
@@ -5,81 +5,46 @@
 
 BASE_DATASET_FOLDER = 'C:/imageDownloads/'
 
-CARS_ITEMS = ["Chevy Sonic",
-    "Ford Fiesta",
-    "Honda Fit",
-    "Hyundai Accent",
+
+CARS_ITEMS = [
     "Kia Rio",
-    "Mazda 2",
-    "Mitsubishi Mirage",
     "Nissan Versa",
-    "Toyota Yaris",
-    "Chevy Cruze",
     "Ford Focus",
     "Honda Civic",
-    "Hyundai Elantra",
-    "Kia Forte",
     "Mazda 3",
-    "Mitsubishi Lancer",
     "Nissan Sentra",
-    "Subaru Impreza",
     "Volkswagen Golf",
-    "Volkswagen Jetta",
-    "Volkswagen Beetle",
-    "Chevy Malibu",
     "Ford Fusion",
     "Honda Accord",
     "Hyundai Sonata",
     "Hyundai Azera",
-    "Kia Optima",
-    "Mazda 6",
     "Nissan Altima",
-    "Subaru Legacy",
     "Toyota Camry",
     "Volkswagen Passat",
     "Chevy Impala",
-    "Ford Taurus",
-    "Kia Cadenza",
-    "Nissan Maxima",
-    "Toyota Avalon"]
+    "Nissan Maxima"
+]
 
-NOT_CARS_ITEMS = ["food",
+NOT_CARS_ITEMS = [
     "building",
     "face",
     "mountain",
-    "animal",
-    "landscape",
-    "office",
-    "person",
-    "window",
-    "tree",
     "sign",
     "door",
-    "bookshelf",
-    "chair",
     "table",
-    "keyboard",
     "head",
-    "clothes",
-    "sports",
     "field",
-    "weather",
     "screenshot",
     "phone",
     "room",
     "electronics",
-    "companies",
-    "actor",
-    "actress",
-    "math",
-    "space",
-    "mammal",
-    "house",
     "kitchen",
-    "store",
-    "map",
-    "washington dc",
-    "ocean"]
+    "child",
+    "floor"
+]
+
+
+
 
 
 
--- edu/gmu/cs/ai/cs580/project/imageclassification/DatasetLoader.py
+++ edu/gmu/cs/ai/cs580/project/imageclassification/DatasetLoader.py
@@ -2,15 +2,23 @@ from PIL import Image
 from edu.gmu.cs.ai.cs580.project.imageclassification import Constants
 from os import listdir
 from os.path import isfile, isdir, join, exists, getsize
+import numpy as np
 
 def load_image(file_path):
     # imgFile = open(file_path, 'rb')
     # imgContent = imgFile.read()
-    imgObj = Image.open(file_path).convert("RGB")
-    bytelist = list(imgObj.getdata())
-    expandedbytelist = list(sum(bytelist, ()))
 
-    return expandedbytelist
+    # The following 4 lines work
+    # imgObj = Image.open(file_path).convert("RGB")
+    # bytelist = list(imgObj.getdata())
+    # expandedbytelist = list(sum(bytelist, ()))
+    # return np.asarray(expandedbytelist)
+
+    with Image.open(file_path) as image:
+        im_arr = np.fromstring(image.tobytes(), dtype = np.uint8)
+        im_arr = im_arr.reshape((image.size[1], image.size[0], 3))
+
+    return im_arr
 
     # imgArr = array(imgObj)
     # test = imgObj.getpixel((35, 23))
@@ -28,21 +36,22 @@ def load_image(file_path):
     # print(newArr2.shape)
 
 
-'''
-     Use 20% of the images for testing. Specify a number (0-4) to indicate which images to add to the test set.
-'''
-def getDataset(testDataSuffixList = [0]):
+def getDataset(testDataSuffixList = [0], predictDataSuffix = [1]):
     baseFolderDir = Constants.BASE_DATASET_FOLDER
     trainingImages = []
     trainingResult = []
     testImages = []
     testResult = []
+    trainSize = 0
+    testSize = 0
+    predictSize = 0
 
     subjectFolders = listdir(baseFolderDir)
 
     for subjectFolder in subjectFolders:
         fullSubjectFolder = baseFolderDir + subjectFolder
         if isdir(fullSubjectFolder):
+
             annotationFilePath = fullSubjectFolder + "/annotation.txt"
             print("Annotation file = " + annotationFilePath)
 
@@ -67,24 +76,30 @@ def getDataset(testDataSuffixList = [0]):
                         if imageNumber % 10 in testDataSuffixList:
                             testImages.append(imageArray)
                             testResult.append(result)
-                        else:
+                            testSize += 1
+                        elif imageNumber % 10 not in predictDataSuffix:
                             trainingImages.append(imageArray)
                             trainingResult.append(result)
+                            trainSize += 1
+                        else:
+                            predictSize += 1
 
                 imageNumber += 1
 
 
-    trainingSet = (trainingImages, trainingResult)
-    testSet = (testImages, testResult)
+    trainingSet = (np.asarray(trainingImages), np.asarray(trainingResult))
+    testSet = (np.asarray(testImages), np.asarray(testResult))
+
+    print("Loaded all images. Training size = " + str(trainSize) + ", Test size = " + str(testSize) + ", Predict size = " + str(predictSize))
 
     return trainingSet, testSet
 
     # onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 
-x, y = getDataset([0])
-print("Training size = " + str(len(x[0])))
-print("Test size = " + str(len(y[0])))
+# x, y = getDataset([0])
+# print("Training size = " + str(len(x[0])))
+# print("Test size = " + str(len(y[0])))
 
 # load_image("C:/imageDownloads/2017-04-14 15-05-00 (Mitsubishi Lancer)/Thumbnails_36_24/0001.jpg")
 
